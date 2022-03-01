5ca880508f74977072c64cf97f8602a8d1815f19
waleson Melo <walesonmelo0000@gmail.com>
2022-03-01 16:09:04 -0300

    ...

--- script.sh
+++ script.sh
@@ -76,13 +76,11 @@
 
 DIR=~/openvpn/ssl-admin
 
-if [ -d "$DIR" ]; then
-    echo "exists" sleep 3
+if [ !-d "$DIR" ]; then
+
+    echo "Initializing..."
+    sleep 3 
+    apt update -y && apt upgrade -y && apt install git -y 
+    git clone https://github.com/shadowbq/ssl-admin.git
 fi
-
-echo "Initializing..."
-sleep 3 
-apt update -y && apt upgrade -y && apt install git -y 
-git clone https://github.com/shadowbq/ssl-admin.git
-
-cd "$DIR" && ./configure && make install
+    cd "$DIR" && ./configure && make install
