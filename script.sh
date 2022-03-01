43c2f779ab638d6a8198e97c995675aa89da02df
waleson Melo <walesonmelo0000@gmail.com>
2022-03-01 18:58:54 -0300

    ...

--- script.sh
+++ script.sh
@@ -90,7 +90,7 @@ then
     cp -a "$PATH_SSL_ADMIN/active/ca.crt" "$PATH_MOVPN/movpn-ca.crt"
     cp -a "$PATH_SSL_ADMIN/active/server.crt" "$PATH_MOVPN/movpn-server.crt"
     cp -a "$PATH_SSL_ADMIN/active/server.key" "$PATH_MOVPN/movpn-server.key"
-    cd "$PATH_MOVPN" && openssl dhparam -out dh2048.pem 2048
+    # cd "$PATH_MOVPN" && openssl dhparam -out dh2048.pem 2048
     
     touch "~/openvpn/client.conf"
 
