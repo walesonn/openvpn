fc467e682cc0d86719c5fb0f886e6c22d320fdfa
waleson Melo <walesonmelo0000@gmail.com>
2022-03-01 21:57:32 -0300

    ...

--- script.sh
+++ script.sh
@@ -88,8 +88,12 @@ then
     groupadd "nobody"
 
     echo 1 > /proc/sys/net/ipv4/ip_forward
-
-    echo "push \"route $gateway 255.255.255.0\"" >> "$SERVER_CONF"
+    result=$(cat basic-udp-server.conf | grep "push")
+    
+    if [[ -z "$result" ]]
+    then
+        echo "push \"route $gateway 255.255.255.0\"" >> "$SERVER_CONF"
+    fi
 
     iptables -t nat -A POSTROUTING -d "137.184.86.0/24" -s "10.0.0.0/24" -j ACCEPT
     iptables -t nat -A POSTROUTING -s "10.0.0.0/24" -o eth0 -j MASQUERADE
